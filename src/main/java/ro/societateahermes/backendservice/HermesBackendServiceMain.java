df438ac5361835335ba0c9ee7a9a0edb6a0538af
mircea216 <panaitemircea378@yahoo.com>
2021-07-29 00:12:39 +0300

    added an encryption for the password and the email in application.properties

--- src/main/java/ro/societateahermes/backendservice/HermesBackendServiceMain.java
+++ src/main/java/ro/societateahermes/backendservice/HermesBackendServiceMain.java
@@ -1,11 +1,13 @@
 package ro.societateahermes.backendservice;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 @EnableScheduling
 public class HermesBackendServiceMain {
